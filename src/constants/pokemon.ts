f673e9f8063108b2be021987be956d0e300fd131
Mangesh636 <mangeshbhardwaj007@gmail.com>
2025-01-15 23:26:19 +0530

    chore: added constants for pokemons (Type, Ability, etc.)

---
+++ src/constants/pokemon.ts
@@ -0,0 +1,396 @@
+// Pokemon Ability
+const PokemonAbilitys = [
+  {
+    label: "Stench",
+    key: "stench",
+  },
+  {
+    label: "Drizzle",
+    key: "drizzle",
+  },
+  {
+    label: "Speed Boost",
+    key: "speed-boost",
+  },
+  {
+    label: "Battle Armor",
+    key: "battle-armor",
+  },
+  {
+    label: "Sturdy",
+    key: "Sturdy",
+  },
+  {
+    label: "Damp",
+    key: "damp",
+  },
+  {
+    label: "Limber",
+    key: "limber",
+  },
+  {
+    label: "Sand Veil",
+    key: "sand-veil",
+  },
+  {
+    label: "Static",
+    key: "static",
+  },
+  {
+    label: "Volt Absorb",
+    key: "volt-absorb",
+  },
+  {
+    label: "Water Absorb",
+    key: "water-absorb",
+  },
+  {
+    label: "Oblivious",
+    key: "oblivious",
+  },
+  {
+    label: "Cloud Nine",
+    key: "cloud-nine",
+  },
+  {
+    label: "Compound Eyes",
+    key: "compound-eyes",
+  },
+  {
+    label: "Insomnia",
+    key: "insomnia",
+  },
+  {
+    label: "Color Change",
+    key: "color-change",
+  },
+  {
+    label: "Immunity",
+    key: "immunity",
+  },
+  {
+    label: "Flash Fire",
+    key: "flash-fire",
+  },
+  {
+    label: "Shield Dust",
+    key: "shield-dust",
+  },
+  {
+    label: "Own Tempo",
+    key: "own-tempo",
+  },
+];
+
+// Pokemon Gender
+const PokemonGenders = [
+  {
+    src: "/icons/female-symbol.svg",
+    alt: "Female Symbol",
+    label: "Female",
+    key: "female",
+  },
+  {
+    src: "/icons/male-symbol.svg",
+    alt: "Male Symbol",
+    label: "Male",
+    key: "male",
+  },
+  {
+    src: "/icons/unisex-symbol.svg",
+    alt: "Unisex Symbol",
+    label: "Genderless",
+    key: "genderless",
+  },
+];
+
+// Pokemon Generation
+const PokemonGenerations = [
+  {
+    label: "Generation I",
+    key: "generation-i",
+  },
+  {
+    label: "Generation II",
+    key: "generation-ii",
+  },
+  {
+    label: "Generation III",
+    key: "generation-iii",
+  },
+  {
+    label: "Generation IV",
+    key: "generation-iv",
+  },
+  {
+    label: "Generation V",
+    key: "generation-v",
+  },
+  {
+    label: "Generation VI",
+    key: "generation-vi",
+  },
+  {
+    label: "Generation VII",
+    key: "generation-vii",
+  },
+  {
+    label: "Generation VIII",
+    key: "generation-viii",
+  },
+  {
+    label: "Generation IX",
+    key: "generation-ix",
+  },
+];
+
+// Pokemon Habitats
+const PokemonHabitats = [
+  {
+    label: "Cave",
+    key: "cave",
+  },
+  {
+    label: "Forest",
+    key: "forest",
+  },
+  {
+    label: "Grassland",
+    key: "grassland",
+  },
+  {
+    label: "Mountain",
+    key: "mountain",
+  },
+  {
+    label: "Rare",
+    key: "rare",
+  },
+  {
+    label: "Rough Terrain",
+    key: "rough-terrain",
+  },
+  {
+    label: "Sea",
+    key: "sea",
+  },
+  {
+    label: "Urban",
+    key: "urban",
+  },
+  {
+    label: "Waters Edge",
+    key: "waters-edge",
+  },
+];
+
+// Pokemon Height
+const PokemonHeight = [
+  {
+    label: "Tall",
+    key: "tall",
+  },
+  {
+    label: "Medium",
+    key: "medium",
+  },
+  {
+    label: "Short",
+    key: "short",
+  },
+];
+
+// Pokemon Moves
+const PokemonMoves = [
+  {
+    label: "Pound",
+    key: "pound",
+  },
+  {
+    label: "Karate Chop",
+    key: "karate-chop",
+  },
+  {
+    label: "Double Slap",
+    key: "double-slap",
+  },
+  {
+    label: "comet Punch",
+    key: "comet-punch",
+  },
+  {
+    label: "Mega Punch",
+    key: "mega-punch",
+  },
+  {
+    label: "Pay Day",
+    key: "pay-day",
+  },
+  {
+    label: "Fire Punch",
+    key: "fire-punch",
+  },
+  {
+    label: "Ice Punch",
+    key: "ice-punch",
+  },
+  {
+    label: "Thunder Punch",
+    key: "thunder-punch",
+  },
+  {
+    label: "Scratch",
+    key: "scratch",
+  },
+  {
+    label: "Vice Grip",
+    key: "vice-grip",
+  },
+  {
+    label: "Guillotine",
+    key: "guillotine",
+  },
+  {
+    label: "Razor Wind",
+    key: "razor-wind",
+  },
+  {
+    label: "Swords Dance",
+    key: "swords-dance",
+  },
+  {
+    label: "Cut",
+    key: "cut",
+  },
+  {
+    label: "Gust",
+    key: "gust",
+  },
+  {
+    label: "Wing Attack",
+    key: "wing-attack",
+  },
+  {
+    label: "Whirlwind",
+    key: "whirlwind",
+  },
+  {
+    label: "Fly",
+    key: "fly",
+  },
+  {
+    label: "Bind",
+    key: "bind",
+  },
+];
+
+// Pokemon Types
+const PokemonTypes = [
+  {
+    label: "Normal",
+    key: "normal",
+  },
+  {
+    label: "Fire",
+    key: "fire",
+  },
+  {
+    label: "Water",
+    key: "water",
+  },
+  {
+    label: "Grass",
+    key: "grass",
+  },
+  {
+    label: "Electric",
+    key: "electric",
+  },
+  {
+    label: "Ice",
+    key: "ice",
+  },
+  {
+    label: "Fighting",
+    key: "fighting",
+  },
+  {
+    label: "Poison",
+    key: "poison",
+  },
+  {
+    label: "Ground",
+    key: "ground",
+  },
+  {
+    label: "Flying",
+    key: "flying",
+  },
+  {
+    label: "Psychic",
+    key: "psychic",
+  },
+  {
+    label: "Bug",
+    key: "bug",
+  },
+  {
+    label: "Rock",
+    key: "rock",
+  },
+  {
+    label: "Ghost",
+    key: "ghost",
+  },
+  {
+    label: "Dragon",
+    key: "dragon",
+  },
+  {
+    label: "Dark",
+    key: "dark",
+  },
+  {
+    label: "Steel",
+    key: "steel",
+  },
+  {
+    label: "Fairy",
+    key: "fairy",
+  },
+  {
+    label: "Stellar",
+    key: "stellar",
+  },
+  {
+    label: "Unknown",
+    key: "unknown",
+  },
+];
+
+// Pokemon Weight
+const PokemonWeight = [
+  {
+    label: "Heavy",
+    key: "heavy",
+  },
+  {
+    label: "Medium",
+    key: "moderate",
+  },
+  {
+    label: "Light",
+    key: "light",
+  },
+];
+
+export {
+  PokemonAbilitys,
+  PokemonGenders,
+  PokemonGenerations,
+  PokemonHabitats,
+  PokemonHeight,
+  PokemonMoves,
+  PokemonTypes,
+  PokemonWeight,
+};
