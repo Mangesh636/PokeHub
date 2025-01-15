0ed8ab5220025b46a5e081d758a4603ace874407
Mangesh636 <mangeshbhardwaj007@gmail.com>
2025-01-15 23:25:14 +0530

    chore: added constants (Navigation and Social)

---
+++ src/constants/index.ts
@@ -0,0 +1,40 @@
+import {
+  Coffee01Icon,
+  Github01Icon,
+  NewTwitterIcon,
+  YoutubeIcon,
+} from "hugeicons-react";
+
+export const NavLinks = [
+  {
+    label: "Home",
+    href: "/",
+  },
+  {
+    label: "About",
+    href: "/about",
+  },
+];
+
+export const FooterSocials = [
+  {
+    icon: NewTwitterIcon,
+    label: "X (Twitter) Icon",
+    href: "https://x.com/CodingMangeshYT",
+  },
+  {
+    icon: Github01Icon,
+    label: "GitHub Icon",
+    href: "https://github.com/Mangesh636/PokeHub",
+  },
+  {
+    icon: YoutubeIcon,
+    label: "YouTube Icon",
+    href: "https://www.youtube.com/@CodeWithMangesh",
+  },
+  {
+    icon: Coffee01Icon,
+    label: "Buy Me Coffee Icon",
+    href: "https://buymeacoffee.com/codewithmangesh",
+  },
+];
