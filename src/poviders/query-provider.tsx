c378d75e151d9c5e0d9a54e855e8e60583faf925
Mangesh636 <mangeshbhardwaj007@gmail.com>
2025-01-17 11:17:07 +0530

    feat: add QueryProviders component for React Query integration

---
+++ src/poviders/query-provider.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useState } from "react";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+export default function QueryProviders({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />{children}</QueryClientProvider>
+  );
+}
